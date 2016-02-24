684bc7c2200f770a9d5307af1ff58a1ece82d4f1
Kevin Malachowski <chowski@google.com>
2016-02-24 14:58:48 -0800

    Add sha and date to the version string.
    Also made the release version a parameter.

--- cmd/cloud_sql_proxy/build.sh
+++ cmd/cloud_sql_proxy/build.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
-VERSION=1.01
+files=$(git status -s)
+if [[ $? != 0 ]]; then
+  echo >&2 "Error running git status"
+  exit 2
+fi
 
+
+if [[ "$1" == "release" ]]; then
+  if [[ "$files" != "" ]]; then
+    echo >&2 "Can't build a release version with local edits; files:"
+    echo >&2 "$files"
+    exit 1
+  fi
+  if [[ "$2" == "" ]]; then
+    echo >&2 "Must provide a version number to use as a second parameter"
+    exit 1
+  fi
+  VERSION="version $2"
+else
+  VERSION="development"
+fi
+
+VERSION+="; sha $(git rev-parse HEAD) built $(date)"
+
+echo "Compiling \"$VERSION\""
 CGO_ENABLED=0 GOOS=linux go build -ldflags "-X main.versionString=$VERSION" -a -installsuffix cgo -o cloud_sql_proxy .
