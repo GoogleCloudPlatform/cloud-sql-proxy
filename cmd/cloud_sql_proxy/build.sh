cc9ba311a75d01bdad3e0d44990e955719538422
Kevin Malachowski <chowski@google.com>
2016-03-22 16:40:23 -0700

    Add the cross-platform build logic for releases

--- cmd/cloud_sql_proxy/build.sh
+++ cmd/cloud_sql_proxy/build.sh
@@ -6,7 +6,7 @@ if [[ $? != 0 ]]; then
   exit 2
 fi
 
-
+RELEASE=0
 if [[ "$1" == "release" ]]; then
   if [[ "$files" != "" ]]; then
     echo >&2 "Can't build a release version with local edits; files:"
@@ -18,11 +18,22 @@ if [[ "$1" == "release" ]]; then
     exit 1
   fi
   VERSION="version $2"
+  RELEASE=1
 else
   VERSION="development"
 fi
 
 VERSION+="; sha $(git rev-parse HEAD) built $(date)"
+echo "Compiling $VERSION..."
 
-echo "Compiling $VERSION"
-CGO_ENABLED=0 GOOS=linux go build -x -ldflags "-X 'main.versionString=$VERSION'" -a -installsuffix cgo -o cloud_sql_proxy .
+if [[ $RELEASE == 0 ]]; then
+  CGO_ENABLED=0 go build -ldflags "-X 'main.versionString=$VERSION'" -a -installsuffix cgo -o cloud_sql_proxy .
+else
+  for OS in windows darwin linux; do
+    for ARCH in amd64 386; do
+      OUT="cloud_sql_proxy.$OS.$ARCH"
+      echo "   Compile $OUT"
+      CGO_ENABLED=0 GOOS=$OS GOARCH=$ARCH go build -ldflags "-X 'main.versionString=$VERSION'" -a -installsuffix cgo -o $OUT .
+    done
+  done
+fi
