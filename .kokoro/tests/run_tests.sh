0e658184a695e4d42eb8a388aa96e0a634429201
kurtisvg <31518063+kurtisvg@users.noreply.github.com>
2020-08-19 15:26:45 -0700

    test: Remove sudo

--- .kokoro/tests/run_tests.sh
+++ .kokoro/tests/run_tests.sh
@@ -21,7 +21,7 @@ cd github/cloud-sql-proxy
 
 # Load in secrets
 if [ -n "$KOKORO_GFILE_DIR" ]; then
-  sudo apt -qq install fuse
+  apt -qq install fuse
   source "${KOKORO_GFILE_DIR}/TEST_SECRETS.sh"
   export GOOGLE_APPLICATION_CREDENTIALS="${KOKORO_GFILE_DIR}/testing-service-account.json"
 fi
