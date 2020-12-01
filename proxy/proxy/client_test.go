70aee818a4db01e89b29b7bd4847cb28b5b2ef6a
kurtisvg <31518063+kurtisvg@users.noreply.github.com>
2020-11-30 17:52:22 -0800

    lint

--- proxy/proxy/client_test.go
+++ proxy/proxy/client_test.go
@@ -288,7 +288,7 @@ func TestRefreshTimer(t *testing.T) {
 }
 
 func TestSyncAtomicAlignment(t *testing.T) {
-	// The sync/atomic pkg has a bug that requires the developer to guarantee 64-bit alignment when using 64-bit functions on 32-bit systems. 
+	// The sync/atomic pkg has a bug that requires the developer to guarantee 64-bit alignment when using 64-bit functions on 32-bit systems.
 	c := &Client{}
 	if a := unsafe.Offsetof(c.ConnectionsCounter); a%64 != 0 {
 		t.Errorf("Client.ConnectionsCounter is not aligned: want %v, got %v", 0, a)
