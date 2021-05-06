b9584eff933b312a0fcb8a373135b605c08765f8
Eno Compton <enocom@google.com>
2021-05-06 15:34:30 -0600

    chore: white space

--- cmd/cloud_sql_proxy/cloud_sql_proxy.go
+++ cmd/cloud_sql_proxy/cloud_sql_proxy.go
@@ -28,7 +28,6 @@ import (
 	"os"
 	"os/signal"
 	"path/filepath"
-	"runtime"
 	"strings"
 	"sync"
 	"syscall"
@@ -465,8 +464,10 @@ func main() {
 
 	// Deprecation warning for darwin 386
 	// TODO(enocom): remove this warning with v1.25.0
-	if runtime.GOOS == "darwin" && runtime.GOARCH == "386" {
-		logging.Infof("WARNING: You are running a Darwin 386 build that is deprecated. The Cloud SQL Auth Proxy will stop providing builds for Darwin 386 as of v1.25.0 (expected August 2021).\nSee https://github.com/GoogleCloudPlatform/cloudsql-proxy/issues/780 for details.")
+	// if runtime.GOOS == "darwin" && runtime.GOARCH == "386" {
+	if true {
+		logging.Infof("WARNING: You are running a Darwin 386 build that is deprecated. The Cloud SQL Auth Proxy will stop providing builds for Darwin 386 as of v1.25.0 (expected August 2021). See https://github.com/GoogleCloudPlatform/cloudsql-proxy/issues/780 for details.")
+		return
 	}
 
 	if *logDebugStdout {
