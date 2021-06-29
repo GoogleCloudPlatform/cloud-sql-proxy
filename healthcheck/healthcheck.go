0d5d6aef0104939fc933e8722d1af4409e7ddc03
Tiffany Xiang <tiffanyx@google.com>
2021-06-29 18:08:50 +0000

    add documentation for package healthcheck

--- healthcheck/healthcheck.go
+++ healthcheck/healthcheck.go
@@ -1,3 +1,5 @@
+// Package healthcheck tests and communicates the health of the Cloud SQL 
+// Proxy Client.
 package healthcheck
 
 import "net/http"
@@ -9,21 +11,21 @@ var (
 
 func initHealthCheck() {
 	http.HandleFunc("/readiness", func(w http.ResponseWriter, r *http.Request) {
-        if ready {
-                w.WriteHeader(200)
-                w.Write([]byte("ok\n"))
-        } else {
-                w.WriteHeader(500)
-        }
+		if ready {
+			w.WriteHeader(200)
+			w.Write([]byte("ok\n"))
+		} else {
+			w.WriteHeader(500)
+		}
 	})
 
 	http.HandleFunc("/liveness", func(w http.ResponseWriter, r *http.Request) {
-        if live {
-                w.WriteHeader(200)
-                w.Write([]byte("ok\n"))
-        } else {
-                w.WriteHeader(500)
-        }
+		if live {
+			w.WriteHeader(200)
+			w.Write([]byte("ok\n"))
+		} else {
+			w.WriteHeader(500)
+		}
 	})
 
 	go http.ListenAndServe(":8080", nil)
