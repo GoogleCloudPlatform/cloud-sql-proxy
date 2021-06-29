fa57263d982ba09397d53569746faa3eb4aeb17f
Tiffany Xiang <tiffanyx@google.com>
2021-06-29 17:28:15 +0000

    create function to initialize HTTP endpoints

--- healthcheck/healthcheck.go
+++ healthcheck/healthcheck.go
@@ -1,6 +1,30 @@
 package healthcheck
 
+import "net/http"
+
 var (
 	live  bool
 	ready bool
 )
+
+func initHealthCheck() {
+	http.HandleFunc("/readiness", func(w http.ResponseWriter, r *http.Request) {
+        if ready {
+                w.WriteHeader(200)
+                w.Write([]byte("ok\n"))
+        } else {
+                w.WriteHeader(500)
+        }
+	})
+
+	http.HandleFunc("/liveness", func(w http.ResponseWriter, r *http.Request) {
+        if live {
+                w.WriteHeader(200)
+                w.Write([]byte("ok\n"))
+        } else {
+                w.WriteHeader(500)
+        }
+	})
+
+	go http.ListenAndServe(":8080", nil)
+}
