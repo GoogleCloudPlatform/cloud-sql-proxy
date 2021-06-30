b3836b657404525dec2801880e6d38e906afa7af
Tiffany Xiang <tiffanyx@google.com>
2021-06-30 20:09:25 +0000

    set up testing

--- healthcheck/healthcheck_test.go
+++ healthcheck/healthcheck_test.go
@@ -1,1 +1,46 @@
 package healthcheck
+
+import (
+	"net/http/httptest"
+	"testing"
+
+	"github.com/GoogleCloudPlatform/cloudsql-proxy/proxy/proxy"
+)
+
+func newClient(mc uint64) *proxy.Client {
+	return &proxy.Client{
+		MaxConnections: mc,
+	}
+}
+
+func newHealthCheck(l bool, r bool, s bool) *HealthCheck {
+	return &HealthCheck{
+		live: l,
+		ready: r,
+		started: s,
+	}
+}
+
+func TestLiveness(t *testing.T) {
+	
+}
+
+func TestUnexpectedTermination(t *testing.T) {
+	
+}
+
+func TestBadStartup(t *testing.T) {
+
+}
+
+func TestNotifyReady(t *testing.T) {
+
+}
+
+func TestReadiness(t *testing.T) {
+
+}
+
+func TestMaxConnections(t *testing.T) {
+
+}
