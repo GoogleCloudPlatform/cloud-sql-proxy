d0be8f7f8cd80f0a41288895035c26177e575ad2
Tiffany Xiang <tiffanyx@google.com>
2021-06-30 23:39:22 +0000

    wrote tests for liveness, bad startup, and successful startup

--- healthcheck/healthcheck_test.go
+++ healthcheck/healthcheck_test.go
@@ -1,6 +1,7 @@
 package healthcheck
 
 import (
+	"net/http"
 	"net/http/httptest"
 	"testing"
 
@@ -22,19 +23,56 @@ func newHealthCheck(l bool, r bool, s bool) *HealthCheck {
 }
 
 func TestLiveness(t *testing.T) {
-	
+	proxyClient := newClient(0)
+	InitHealthCheck(proxyClient)
+
+	resp, err := http.Get("/liveness")
+	if err != nil {
+		t.Errorf("failed to GET from /liveness")
+	}
+	if resp.StatusCode != 200 {
+		t.Errorf("got status code %v instead of 200", resp.StatusCode)
+	}
+	if resp.Status != "ok\n" {
+		t.Errorf("got status %v instead of \"ok\\n\"", resp.Status)
+	}
 }
 
 func TestUnexpectedTermination(t *testing.T) {
-	
+
 }
 
 func TestBadStartup(t *testing.T) {
+	proxyClient := newClient(0)
+	InitHealthCheck(proxyClient)
 
+	resp, err := http.Get("/readiness")
+	if err != nil {
+		t.Errorf("failed to GET from /readiness")
+	}
+	if resp.StatusCode != 500 {
+		t.Errorf("got status code %v instead of 500", resp.StatusCode)
+	}
+	if resp.Status != "error\n" {
+		t.Errorf("got status %v instead of \"error\\n\"", resp.Status)
+	}
 }
 
-func TestNotifyReady(t *testing.T) {
+func TestSuccessfulStartup(t *testing.T) {
+	proxyClient := newClient(0)
+	hc := InitHealthCheck(proxyClient)
+	NotifyReady(hc)
 
+	resp, err := http.Get("/readiness")
+	if err != nil {
+		t.Errorf("failed to GET from /readiness")
+	}
+	if resp.StatusCode != 200 {
+		t.Errorf("got status code %v instead of 200", resp.StatusCode)
+	}
+	if resp.Status != "ok\n" {
+		t.Errorf("got status %v instead of \"ok\\n\"", resp.Status)
+	}
 }
 
 func TestReadiness(t *testing.T) {
