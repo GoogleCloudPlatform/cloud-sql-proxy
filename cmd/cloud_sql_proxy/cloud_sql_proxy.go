76f0a6b963322539ebb02f850a7bf99924756f33
Tiffany Xiang <tiffanyx@google.com>
2021-07-29 17:02:07 +0000

    fix flag

--- cmd/cloud_sql_proxy/cloud_sql_proxy.go
+++ cmd/cloud_sql_proxy/cloud_sql_proxy.go
@@ -135,7 +135,7 @@ unavailable.`,
 
 	// Settings for healthcheck
 	useHTTPHealthCheck = flag.Bool("use_http_health_check", false, "When set, creates an HTTP server that checks and communicates the health of the proxy client.")
-	healthCheckPort = flag.String("health_check_port", "8090", "When applicable, health checks take place on this port number. Defaults to 8090.")
+	healthCheckPort = flag.String("health_check_port", "8090", "Health checks take place on this port number. Defaults to 8090.")
 )
 
 const (
