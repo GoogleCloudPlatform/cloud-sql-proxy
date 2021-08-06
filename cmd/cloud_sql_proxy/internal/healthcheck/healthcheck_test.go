69885768125523bc22ded96a5ba1f07e3c8d79a3
Tiffany Xiang <tiffanyx@google.com>
2021-08-06 21:54:54 +0000

    convert dialing tests into table driven tests

--- cmd/cloud_sql_proxy/internal/healthcheck/healthcheck_test.go
+++ cmd/cloud_sql_proxy/internal/healthcheck/healthcheck_test.go
@@ -145,54 +145,40 @@ func TestMaxConnectionsReached(t *testing.T) {
 	}
 }
 
-// Test to verify that when a client has one instance and dialing it returns an error,
-// the readiness endpoint writes http.StatusServiceUnavailable.
-func TestSingleInstanceFail(t *testing.T) {
+// Test to verify that when dialing instance(s) returns an error, the readiness endpoint 
+// writes http.StatusServiceUnavailable.
+func TestDialFail(t *testing.T) {
+    tests := map[string]struct {
+        insts []string
+	}{
+		"Single instance": {insts: []string{"project:region:instance"}},
+		"Multiple instances": {insts: []string{"project:region:instance-1", "project:region:instance-2", "project:region:instance-3"}},
+	}
+
 	c := &proxy.Client{
 		Certs: &fakeCertSource{},
 		Dialer: func(string, string) (net.Conn, error) {
 			return nil, errors.New("error")
 		},
 	}
-	s, err := healthcheck.NewServer(c, testPort, []string{"instance-name"})
-	if err != nil {
-		t.Fatalf("Could not initialize health check: %v", err)
-	}
-	defer s.Close(context.Background())
-	s.NotifyStarted()
 
-	resp, err := http.Get("http://localhost:" + testPort + readinessPath)
-	if err != nil {
-		t.Fatalf("HTTP GET failed: %v", err)
-	}
-	if resp.StatusCode != http.StatusServiceUnavailable {
-		t.Errorf("Got status code %v instead of %v", resp.StatusCode, http.StatusServiceUnavailable)
-	}
-}
-
-// Test to verify that when a client has multiple instances and dialing them returns an error,
-// the readiness endpoint writes http.StatusServiceUnavailable.
-func TestMultiInstanceFail(t *testing.T) {
-	c := &proxy.Client{
-		Certs: &fakeCertSource{},
-		Dialer: func(string, string) (net.Conn, error) {
-			return nil, errors.New("error")
-		},
-	}
-	s, err := healthcheck.NewServer(c, testPort, []string{"instance-1", "instance-2", "instance-3"})
-	if err != nil {
-		t.Fatalf("Could not initialize health check: %v", err)
-	}
-	defer s.Close(context.Background())
-
-	s.NotifyStarted()
-
-	resp, err := http.Get("http://localhost:" + testPort + readinessPath)
-	if err != nil {
-		t.Fatalf("HTTP GET failed: %v", err)
-	}
-	if resp.StatusCode != http.StatusServiceUnavailable {
-		t.Errorf("Got status code %v instead of %v", resp.StatusCode, http.StatusServiceUnavailable)
+	for name, test := range tests {
+		func() {
+			s, err := healthcheck.NewServer(c, testPort, test.insts)
+			if err != nil {
+				t.Fatalf("%v: Could not initialize health check: %v", name, err)
+			}
+			defer s.Close(context.Background())
+			s.NotifyStarted()
+		
+			resp, err := http.Get("http://localhost:" + testPort + readinessPath)
+			if err != nil {
+				t.Fatalf("%v: HTTP GET failed: %v", name, err)
+			}
+			if resp.StatusCode != http.StatusServiceUnavailable {
+				t.Errorf("%v: Got status code %v instead of %v", name, resp.StatusCode, http.StatusServiceUnavailable)
+			}
+		}()
 	}
 }
 
