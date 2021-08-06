989d6d2a328673a1cc6a866cb490598b79fbab23
Tiffany Xiang <tiffanyx@google.com>
2021-08-06 22:47:12 +0000

    revert to separate tests

--- tests/healthcheck_test.go
+++ tests/healthcheck_test.go
@@ -42,15 +42,12 @@ func waitForStart(ctx context.Context) {
 	}
 }
 
-// Test to verify that when all instances can be dialed successfully, the readiness endpoint
-// writes http.StatusOK.
-func TestDialPass(t *testing.T) {
-	tests := map[string]struct {
-		insts []string
-		ports []int
-	}{
-		"Single instance":    {insts: []string{*mysqlConnName}, ports: []int{mysqlPort}},
-		"Multiple instances": {insts: []string{*mysqlConnName, *postgresConnName}, ports: []int{mysqlPort, postgresPort}},
+// Test to verify that when a proxy client serves one instance that can successfully be dialed,
+// the readiness endpoint serves http.StatusOK.
+func TestSingleInstanceDial(t *testing.T) {
+	switch "" {
+	case *mysqlConnName:
+		t.Fatal("'mysql_conn_name' not set")
 	}
 
 	binPath, err := compileProxy()
@@ -59,40 +56,64 @@ func TestDialPass(t *testing.T) {
 	}
 	defer os.RemoveAll(binPath)
 
-	for name, test := range tests {
-		for _, inst := range test.insts {
-			if inst == "" {
-				t.Fatalf("%v: connection name not set", name)
-			}
-		}
+	var args []string
+	args = append(args, fmt.Sprintf("-instances=%s=tcp:%d", *mysqlConnName, mysqlPort), "-use_http_health_check")
 
-		func() {
-			arg := "-instances="
-			for i, inst := range test.insts {
-				if i > 0 {
-					arg += ","
-				}
-				arg += fmt.Sprintf("%s=tcp:%d", inst, test.ports[i])
-			}
+	cmd := exec.Command(binPath, args...)
+	err = cmd.Start()
+	if err != nil {
+		t.Fatalf("Failed to start proxy: %s", err)
+	}
+	defer cmd.Process.Kill()
 
-			cmd := exec.Command(binPath, arg, "-use_http_health_check")
-			err = cmd.Start()
-			if err != nil {
-				t.Fatalf("%v: Failed to start proxy: %s", name, err)
-			}
-			defer cmd.Process.Kill()
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(10 * time.Second))
+	defer cancel()
+	waitForStart(ctx)
 
-			ctx, cancel := context.WithTimeout(context.Background(), time.Duration(10*time.Second))
-			defer cancel()
-			waitForStart(ctx)
+	resp, err := http.Get("http://localhost:" + testPort + readinessPath)
+	if err != nil {
+		t.Fatalf("HTTP GET failed: %v", err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("%v returned status code %v instead of %v", readinessPath, resp.StatusCode, http.StatusOK)
+	}
+}
 
-			resp, err := http.Get("http://localhost:" + testPort + readinessPath)
-			if err != nil {
-				t.Fatalf("%v: HTTP GET failed: %v", name, err)
-			}
-			if resp.StatusCode != http.StatusOK {
-				t.Errorf("%v: Got status code %v instead of %v", name, resp.StatusCode, http.StatusOK)
-			}
-		}()
+// Test to verify that when a proxy client serves multiple instances that can all be successfully dialed,
+// the readiness endpoint serves http.StatusOK.
+func TestMultiInstanceDial(t *testing.T) {
+	switch "" {
+	case *mysqlConnName:
+		t.Fatal("'mysql_conn_name' not set")
+	case *postgresConnName:
+		t.Fatal("'postgres_conn_name' not set")
+	}
+
+	binPath, err := compileProxy()
+	if err != nil {
+		t.Fatalf("Failed to compile proxy: %s", err)
+	}
+	defer os.RemoveAll(binPath)
+
+	var args []string
+	args = append(args, fmt.Sprintf("-instances=%s=tcp:%d,%s=tcp:%d", *mysqlConnName, mysqlPort, *postgresConnName, postgresPort), "-use_http_health_check")
+
+	cmd := exec.Command(binPath, args...)
+	err = cmd.Start()
+	if err != nil {
+		t.Fatalf("Failed to start proxy: %s", err)
+	}
+	defer cmd.Process.Kill()
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(10 * time.Second))
+	defer cancel()
+	waitForStart(ctx)
+
+	resp, err := http.Get("http://localhost:" + testPort + readinessPath)
+	if err != nil {
+		t.Fatalf("HTTP GET failed: %v", err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("%v returned status code %v instead of %v", readinessPath, resp.StatusCode, http.StatusOK)
 	}
 }
