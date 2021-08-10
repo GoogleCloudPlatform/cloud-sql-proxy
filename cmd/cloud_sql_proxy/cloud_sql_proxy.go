cd8f5731cdb038fdabc6ce73bf98eba5d6bfd92e
Eno Compton <enocom@google.com>
2021-08-10 11:48:17 -0600

    Ensure defer statements are run

--- cmd/cloud_sql_proxy/cloud_sql_proxy.go
+++ cmd/cloud_sql_proxy/cloud_sql_proxy.go
@@ -442,29 +442,12 @@ func gcloudProject() ([]string, error) {
 	return []string{cfg.Configuration.Properties.Core.Project}, nil
 }
 
-// Main executes the main function of the proxy, allowing it to be called from tests.
-//
-// Setting timeout to a value greater than 0 causes the process to panic after
-// that amount of time. This is to sidestep an issue where sending a Signal to
-// the process (via the SSH library) doesn't seem to have an effect, and
-// closing the SSH session causes the process to get leaked. This timeout will
-// at least cause the proxy to exit eventually.
-func Main(timeout time.Duration) {
-	if timeout > 0 {
-		go func() {
-			time.Sleep(timeout)
-			panic("timeout exceeded")
-		}()
-	}
-	main()
-}
-
-func main() {
+func startProxy() error {
 	flag.Parse()
 
 	if *version {
 		fmt.Println("Cloud SQL Auth proxy:", semanticVersion())
-		return
+		return nil
 	}
 
 	if *logDebugStdout {
@@ -479,7 +462,7 @@ func main() {
 		cleanup, err := logging.EnableStructuredLogs(*logDebugStdout, *verbose)
 		if err != nil {
 			logging.Errorf("failed to enable structured logs: %v", err)
-			os.Exit(1)
+			return err
 		}
 		defer cleanup()
 	}
@@ -501,7 +484,7 @@ func main() {
 	if *host != "" && !strings.HasSuffix(*host, "/") {
 		logging.Errorf("Flag host should always end with /")
 		flag.PrintDefaults()
-		return
+		return errors.New("invalid command")
 	}
 
 	// TODO: needs a better place for consolidation
@@ -520,36 +503,36 @@ func main() {
 			logging.Infof("Using gcloud's active project: %v", projList)
 		} else if gErr, ok := err.(*util.GcloudError); ok && gErr.Status == util.GcloudNotFound {
 			logging.Errorf("gcloud is not in the path and -instances and -projects are empty")
-			os.Exit(1)
+			return err
 		} else {
 			logging.Errorf("unable to retrieve the active gcloud project and -instances and -projects are empty: %v", err)
-			os.Exit(1)
+			return err
 		}
 	}
 
 	onGCE := metadata.OnGCE()
 	if err := checkFlags(onGCE); err != nil {
 		logging.Errorf(err.Error())
-		os.Exit(1)
+		return err
 	}
 
 	ctx, cancel := context.WithCancel(context.Background())
 	client, tokSrc, err := authenticatedClient(ctx)
 	if err != nil {
 		logging.Errorf(err.Error())
-		os.Exit(1)
+		return err
 	}
 
 	ins, err := listInstances(ctx, client, projList)
 	if err != nil {
 		logging.Errorf(err.Error())
-		os.Exit(1)
+		return err
 	}
 	instList = append(instList, ins...)
 	cfgs, err := CreateInstanceConfigs(*dir, *useFuse, instList, *instanceSrc, client, *skipInvalidInstanceConfigs)
 	if err != nil {
 		logging.Errorf(err.Error())
-		os.Exit(1)
+		return err
 	}
 
 	// We only need to store connections in a ConnSet if FUSE is used; otherwise
@@ -590,7 +573,7 @@ func main() {
 		hc, err = healthcheck.NewServer(proxyClient, *healthCheckPort)
 		if err != nil {
 			logging.Errorf("Could not initialize health check server: %v", err)
-			os.Exit(1)
+			return err
 		}
 		defer hc.Close(ctx)
 	}
@@ -601,7 +584,7 @@ func main() {
 		c, fuse, err := fuse.NewConnSrc(*dir, *fuseTmp, proxyClient, connset)
 		if err != nil {
 			logging.Errorf("Could not start fuse directory at %q: %v", *dir, err)
-			os.Exit(1)
+			return err
 		}
 		connSrc = c
 		defer fuse.Close()
@@ -627,7 +610,7 @@ func main() {
 		c, err := WatchInstances(*dir, cfgs, updates, client)
 		if err != nil {
 			logging.Errorf(err.Error())
-			os.Exit(1)
+			return err
 		}
 		connSrc = c
 	}
@@ -641,8 +624,8 @@ func main() {
 	signals := make(chan os.Signal, 1)
 	signal.Notify(signals, syscall.SIGTERM, syscall.SIGINT)
 
+	shutdown := make(chan error, 1)
 	go func() {
-		defer cancel()
 		<-signals
 		logging.Infof("Received TERM signal. Waiting up to %s before terminating.", *termTimeout)
 		go func() {
@@ -651,11 +634,14 @@ func main() {
 			}
 		}()
 
+		cancel()
 		err := proxyClient.Shutdown(*termTimeout)
 		if err != nil {
 			logging.Errorf("Error during SIGTERM shutdown: %v", err)
-			os.Exit(1)
+			shutdown <- err
+			return
 		}
+		close(shutdown)
 	}()
 
 	// If running under systemd with Type=notify, we'll send a message to the
@@ -667,4 +653,15 @@ func main() {
 		}
 	}()
 	proxyClient.RunContext(ctx, connSrc)
+	if err, ok := <-shutdown; ok {
+		fmt.Println("here")
+		return err
+	}
+	return nil
+}
+
+func main() {
+	if err := startProxy(); err != nil {
+		os.Exit(1)
+	}
 }
