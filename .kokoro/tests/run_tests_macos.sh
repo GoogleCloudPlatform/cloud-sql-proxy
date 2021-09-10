73e7a163249bfd4c9e22a3e80a4e822f71494a76
Eno Compton <enocom@google.com>
2021-09-10 15:53:12 -0600

    try again

--- .kokoro/tests/run_tests_macos.sh
+++ .kokoro/tests/run_tests_macos.sh
@@ -33,7 +33,7 @@ if [ -n "$KOKORO_GFILE_DIR" ]; then
   echo Making tmp writable for tests
   sudo chmod 1777 /private/tmp
   sudo chmod 1777 /tmp
-  ls -lax /
+  ls -la /
 fi
 
 # On macOS, the default $TMPDIR is too long for suitable use due to the unix socket length limits
