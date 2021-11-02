6986d3dd8cdca5a8da8e3057e98fef0ff7c663d6
Eno Compton <enocom@google.com>
2021-11-02 15:50:14 -0600

    chore: bump to latest dev version

--- cmd/cloud_sql_proxy/cloud_sql_proxy.go
+++ cmd/cloud_sql_proxy/cloud_sql_proxy.go
@@ -272,7 +272,7 @@ Information for all flags:
 var defaultTmp = filepath.Join(os.TempDir(), "cloudsql-proxy-tmp")
 
 // versionString indiciates the version of the proxy currently in use.
-var versionString = "1.27.0"
+var versionString = "1.27.1-dev"
 
 // metadataString indiciates additional build or distribution metadata.
 var metadataString = ""
