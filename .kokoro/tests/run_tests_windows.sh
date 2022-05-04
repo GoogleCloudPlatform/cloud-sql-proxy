62e603d3c467e0b8dce061c7640983cf62d53519
Shubha Rajan <shubhadayini@google.com>
2022-05-04 16:12:32 -0700

    Update run_tests_windows.sh

--- .kokoro/tests/run_tests_windows.sh
+++ .kokoro/tests/run_tests_windows.sh
@@ -16,8 +16,6 @@
 # `-e` enables the script to automatically fail when a command fails
 set -e
 
-export GO111MODULE=on
-
 # Kokoro setup
 if [ -n "$KOKORO_GFILE_DIR" ]; then
   # Move into project directory
