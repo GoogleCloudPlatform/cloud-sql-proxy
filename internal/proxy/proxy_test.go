bfc5a712e64250d5ea245599cb7a0d844c887272
Eno Compton <enocom@google.com>
2022-07-07 11:11:43 -0600

    feat: add max connections flag (#1240)
    The max connections flags allows callers to limit the number of outgoing
    connections to the Cloud SQL backend. Max connections are tracked on a
    global level across all instances.

--- internal/proxy/proxy_test.go
+++ internal/proxy/proxy_test.go
@@ -17,34 +17,45 @@ package proxy_test
 import (
 	"context"
 	"errors"
+	"io"
 	"io/ioutil"
 	"net"
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
 	"testing"
 	"time"
 
 	"cloud.google.com/go/cloudsqlconn"
-	"github.com/GoogleCloudPlatform/cloudsql-proxy/v2/cloudsql"
 	"github.com/GoogleCloudPlatform/cloudsql-proxy/v2/internal/proxy"
 	"github.com/spf13/cobra"
 )
 
 type fakeDialer struct {
-	cloudsql.Dialer
+	mu        sync.Mutex
+	dialCount int
 }
 
-func (fakeDialer) Close() error {
+func (*fakeDialer) Close() error {
 	return nil
 }
 
-func (fakeDialer) Dial(ctx context.Context, inst string, opts ...cloudsqlconn.DialOption) (net.Conn, error) {
-	conn, _ := net.Pipe()
-	return conn, nil
+func (f *fakeDialer) dialAttempts() int {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+	return f.dialCount
 }
 
-func (fakeDialer) EngineVersion(_ context.Context, inst string) (string, error) {
+func (f *fakeDialer) Dial(ctx context.Context, inst string, opts ...cloudsqlconn.DialOption) (net.Conn, error) {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+	f.dialCount++
+	c1, _ := net.Pipe()
+	return c1, nil
+}
+
+func (*fakeDialer) EngineVersion(_ context.Context, inst string) (string, error) {
 	switch {
 	case strings.Contains(inst, "pg"):
 		return "POSTGRES_14", nil
@@ -227,7 +238,7 @@ func TestClientInitialization(t *testing.T) {
 
 	for _, tc := range tcs {
 		t.Run(tc.desc, func(t *testing.T) {
-			tc.in.Dialer = fakeDialer{}
+			tc.in.Dialer = &fakeDialer{}
 			c, err := proxy.NewClient(ctx, &cobra.Command{}, tc.in)
 			if err != nil {
 				t.Fatalf("want error = nil, got = %v", err)
@@ -258,6 +269,51 @@ func TestClientInitialization(t *testing.T) {
 	}
 }
 
+func TestClientLimitsMaxConnections(t *testing.T) {
+	d := &fakeDialer{}
+	in := &proxy.Config{
+		Addr: "127.0.0.1",
+		Port: 5000,
+		Instances: []proxy.InstanceConnConfig{
+			{Name: "proj:region:pg"},
+		},
+		MaxConnections: 1,
+		Dialer:         d,
+	}
+	c, err := proxy.NewClient(context.Background(), &cobra.Command{}, in)
+	if err != nil {
+		t.Fatalf("proxy.NewClient error: %v", err)
+	}
+	defer c.Close()
+	go c.Serve(context.Background())
+
+	conn1, err1 := net.Dial("tcp", "127.0.0.1:5000")
+	if err1 != nil {
+		t.Fatalf("net.Dial error: %v", err1)
+	}
+	defer conn1.Close()
+
+	conn2, err2 := net.Dial("tcp", "127.0.0.1:5000")
+	if err2 != nil {
+		t.Fatalf("net.Dial error: %v", err1)
+	}
+	defer conn2.Close()
+
+	// try to read to check if the connection is closed
+	// wait only a second for the result (since nothing is writing to the
+	// socket)
+	conn2.SetReadDeadline(time.Now().Add(time.Second))
+	_, rErr := conn2.Read(make([]byte, 1))
+	if rErr != io.EOF {
+		t.Fatalf("conn.Read should return io.EOF, got = %v", rErr)
+	}
+
+	want := 1
+	if got := d.dialAttempts(); got != want {
+		t.Fatalf("dial attempts did not match expected, want = %v, got = %v", want, got)
+	}
+}
+
 func TestClientClosesCleanly(t *testing.T) {
 	in := &proxy.Config{
 		Addr: "127.0.0.1",
@@ -265,7 +321,7 @@ func TestClientClosesCleanly(t *testing.T) {
 		Instances: []proxy.InstanceConnConfig{
 			{Name: "proj:reg:inst"},
 		},
-		Dialer: fakeDialer{},
+		Dialer: &fakeDialer{},
 	}
 	c, err := proxy.NewClient(context.Background(), &cobra.Command{}, in)
 	if err != nil {
@@ -292,7 +348,7 @@ func TestClosesWithError(t *testing.T) {
 		Instances: []proxy.InstanceConnConfig{
 			{Name: "proj:reg:inst"},
 		},
-		Dialer: errorDialer{},
+		Dialer: &errorDialer{},
 	}
 	c, err := proxy.NewClient(context.Background(), &cobra.Command{}, in)
 	if err != nil {
@@ -346,9 +402,8 @@ func TestClientInitializationWorksRepeatedly(t *testing.T) {
 		Instances: []proxy.InstanceConnConfig{
 			{Name: "proj:region:pg"},
 		},
-		Dialer: fakeDialer{},
+		Dialer: &fakeDialer{},
 	}
-
 	c, err := proxy.NewClient(ctx, &cobra.Command{}, in)
 	if err != nil {
 		t.Fatalf("want error = nil, got = %v", err)
