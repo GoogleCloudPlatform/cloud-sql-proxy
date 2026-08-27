933877de0c8569a642d4f13ae51a6c63e4358c0a
Jonathan Hess <hessjc@google.com>
2026-08-27 17:31:59 +0000

    fix: check context cancellation in serveSocketMount

--- internal/proxy/proxy.go
+++ internal/proxy/proxy.go
@@ -790,8 +790,13 @@ func (c *Client) Close() error {
 
 // serveSocketMount persistently listens to the socketMounts listener and proxies connections to a
 // given Cloud SQL instance.
-func (c *Client) serveSocketMount(_ context.Context, s *socketMount) error {
+func (c *Client) serveSocketMount(ctx context.Context, s *socketMount) error {
 	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		default:
+		}
 		cConn, err := s.Accept()
 		if err != nil {
 			if nerr, ok := err.(net.Error); ok && nerr.Timeout() {
